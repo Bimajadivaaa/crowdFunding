f72e077e3050bb2f45ad0b4ed0cb4d0c319cb23a
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2023-10-24 16:17:08 +0700

    commit frontend

---
+++ web3/hardhat.config.js
@@ -0,0 +1,36 @@
+require("@matterlabs/hardhat-zksync-solc");
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    defaultNetwork : 'bsc',
+    networks : {
+      hardhat : {},
+      bscTestnet: {
+        url: "https://bsc-testnet.publicnode.com",
+        accounts : [`0x${process.env.PRIVATE_KEY}`],
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
