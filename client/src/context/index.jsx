a3def7bff3a53a690d577c076e1ba85f83e333b7
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2023-11-01 10:54:17 +0700

    add getcampaigns

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -39,14 +39,32 @@ export const StateContextProvider = ({ children }) => {
       console.log("contract call failure", error);
     }
   };
+
+  const getCampaigns = async() => {
+    const campaigns = await contract.call('getCampaigns');
+    const parsedCampaigns = campaigns.map((campaign) => ({
+      owner : campaign.owner,
+      title : campaign.title,
+      description : campaign.description,
+      target : ethers.utils.formatEther(campaign.target.toString()),
+      deadline : campaign.deadline.toNumber(),
+      amountCollected : ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image : campaign.image,
+      pId : campaign.i 
+    }))
+    // console.log(parsedCampaigns);
+    return parsedCampaigns;
+  }
  
   return (
     <StateContext.Provider
       value={{ 
         address, 
         contract, 
-        connect, 
-        createCampaign: publishCampaign }}
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns
+      }}
     >
       {children}
     </StateContext.Provider>
