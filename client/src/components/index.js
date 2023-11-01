8de81f7a43df45d133828805b492f9c8121f5c41
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2023-11-01 19:05:55 +0700

    add campaign details

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,4 @@ export { default as CustomButton } from "./CustomButton";
 export { default as FormField } from "./FormField";
 export { default as DisplayCampaign } from "./DisplayCampaign";
 export { default as FundCard } from "./FundCard.jsx";
+export { default as CountBox } from "./CountBox";
--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -33,37 +33,76 @@ export const StateContextProvider = ({ children }) => {
           new Date(form.deadline).getTime(),
           0,
           form.image,
-        ]});
+        ],
+      });
       console.log("contract call success", data);
     } catch (error) {
       console.log("contract call failure", error);
     }
   };
 
-  const getCampaigns = async() => {
-    const campaigns = await contract.call('getCampaigns');
-    const parsedCampaigns = campaigns.map((campaign) => ({
-      owner : campaign.owner,
-      title : campaign.title,
-      description : campaign.description,
-      target : ethers.utils.formatEther(campaign.target.toString()),
-      deadline : campaign.deadline.toNumber(),
-      amountCollected : ethers.utils.formatEther(campaign.amountCollected.toString()),
-      image : campaign.image,
-      pId : campaign.i 
-    }))
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCampaigns");
+    const parsedCampaigns = campaigns.map((campaign, index) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: index,
+    }));
     // console.log(parsedCampaigns);
     return parsedCampaigns;
+  };
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filterCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+    return filterCampaigns;
+  };
+
+  const donate = async(pId, amount) => {
+    const etherValue = ethers.utils.parseEther(amount)
+    const data = await contract.call('donateToCampaign', [pId], {value: etherValue});
+
+    return data;
   }
- 
+
+  const getDonations = async(pId) => {
+    const donations = await contract.call('getDonators', [pId]);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for(let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator : donations[0][i],
+        donation : ethers.utils.formatEther(donations[1][i].toString())
+      })
+    }
+
+    return parsedDonations;
+
+  }
+
   return (
     <StateContext.Provider
-      value={{ 
-        address, 
-        contract, 
+      value={{
+        address,
+        contract,
         connect,
         createCampaign: publishCampaign,
-        getCampaigns
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations
       }}
     >
       {children}
